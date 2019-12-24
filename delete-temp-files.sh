dc3770eddfa186a4c51675a84523dda49aa50aa5
lilizhao216902 <lilizhao216902@sogou-inc.com>
2019-12-24 10:37:11 +0800

    init

---
+++ delete-temp-files.sh
@@ -0,0 +1,4 @@
+rm -f *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex
+
+
+rm -f *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk
